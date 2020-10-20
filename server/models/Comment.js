a47e3f60cc73e33edc17ac49627ecef2bcdb3a06
Walker-Walker <awalker0@gmail.com>
2020-10-19 19:01:15 -0700

    Required/exported  User and Comment mongoose  models  in  index file in models directory, comment and user models have also been added. Initialized Package.json for server directory included following dependencies: mongoose, stripe, express, apollo-server-express, jsonwebtoken, and bcrypt. Moving on to graphQl schemas...

---
+++ server/models/Comment.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const User = require("./User");
+
+const commentSchema = new Schema({
+  // mongoose default creates an unique id?
+  commentText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280,
+  },
+
+  recipeId: {
+    type: String,
+    required: true,
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timeStamp) => moment(timeStamp).format("MMM DD, YYYY [at] hh:mm a"),
+  },
+
+  user: [User.Schema],
+});
+
+module.exports = Comment;
