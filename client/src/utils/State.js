6408dffbd8395e06433134301cb3001220b78818
Theresa Rutledge <t.roserutledge@gmail.com>
2020-10-21 11:28:14 -0700

    actions, reducers,state

--- client/src/utils/State.js
+++ client/src/utils/State.js
@@ -1,1 +1,20 @@
 // Theresa
+import React, {createContext, useContext} from 'react';
+import {useRecipeReducer} from './reducers';
+
+const StoreContext = createContext();
+const {Provider} = StoreContext;
+
+const StoreProvider = (props) => {
+    const [state,dispatch] = useRecipeReducer({
+        ingredients:[],
+        recipes:[]
+    });
+    return <Provider value={[state,dispatch]} {...props} />
+};
+
+const useStoreContext = () => {
+    return useContext(StoreContext);
+};
+
+export {StoreProvider, useStoreContext};
