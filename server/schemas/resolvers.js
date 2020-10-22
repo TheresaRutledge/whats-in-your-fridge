ea155bc340ad5089b3007ba046d58a97f086503e
Walker-Walker <awalker0@gmail.com>
2020-10-21 23:23:55 -0700

    added addComment, UpdateComment,and deleteComment Mutations in resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,54 +1,63 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { User, Comment } = require('../models');
-const { signToken } = require('../utils/auth');
-const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
-
-
+const { AuthenticationError } = require("apollo-server-express");
+const { User, Comment } = require("../models");
+const { signToken } = require("../utils/auth");
+const stripe = require("stripe")("sk_test_4eC39HqLyjWDarjtT1zdp7dc");
 
 const resolvers = {
-    Query: {
-        user: async (parent, args, context) => {
-            if(context.user) {
-                const user = await User.findById(context.user._id)
-                return user;
-            }
-        },
-
-        comment: async (parent, args, context) => {
-            if(context.Comment) {
-                const comment = await Comment.findById(context.comment._id)
-                return comment;
-            }
-        }
-
-
+  Query: {
+    user: async (parent, args, context) => {
+      if (context.user) {
+        const user = await User.findById(context.user._id);
+        return user;
+      }
     },
 
-    Mutation: {
-        addUser: async (parent, args) => {
-            const user = await User.create(args);
-            const token = signToken(user);
-            return { token, user };
-        },
-        //match authentication methods/names with teammeates...signToken
-        login: async (parent, { email, password }) => {
-            const user = await User.findOne({ email });
+    comment: async (parent, args, context) => {
+      if (context.Comment) {
+        const comment = await Comment.findById(context.comment._id);
+        return comment;
+      }
+    },
+  },
 
-            if (!user) {
-                throw new AuthenticationError('Incorrect credentials');
-            }
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+    //match authentication methods/names with teammeates...signToken
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
 
-            const correctPw = await user.isCorrectPassword(password);
+      if (!user) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
 
-            if(!correctPw) {
-                throw new AuthenticationError('Oops! Wrong Password')
-            }
+      const correctPw = await user.isCorrectPassword(password);
 
-            const token = signToken(user);
+      if (!correctPw) {
+        throw new AuthenticationError("Oops! Wrong Password");
+      }
 
-            return { token, user };
-        }
+      const token = signToken(user);
+
+      return { token, user };
+    },
+
+    addComment: async (parent, args) => {
+      const comment = await Comment.create(args);
+      return comment;
+    },
+
+    updateComment: async (parent, {_id}) => {
+        return await Comment.findByIdAndUpdate(_id);
+    },
+
+    deleteComment: async (parent, {_id}) => {
+        return await Comment.findByIdAndDelete(_id);
     }
-}
+  },
+};
 
 module.exports = resolvers;
