cef9510ecf406c1022a68273ce9c5d0fd2bba63b
Theresa Rutledge <t.roserutledge@gmail.com>
2020-10-23 11:34:53 -0700

    app.js works, required auth file and tweaks to existing components:login,signup,nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,2 +1,49 @@
-// Theresa
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+import Donate from "./pages/Donate";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import NoMatch from "./pages/NoMatch";
+import Profile from "./pages/Profile";
 import Navbar from './components/Navbar';
+
+import { StoreProvider } from "./utils/State";
+
+const client = new ApolloClient({
+    request: (operation) => {
+        const token = localStorage.getItem('id_token')
+        operation.setContext({
+            headers: {
+                authorization: token ? `Bearer ${token}` : ''
+            }
+        })
+    },
+    uri: '/graphql',
+})
+
+
+function App() {
+    return (
+        <ApolloProvider client={client}>
+            <Router>
+                <div>
+                    <StoreProvider>
+                        <Navbar />
+                        <Switch>
+                            <Route exact path='/' component={Home} />
+                            {/* <Route exact path='/login' component={Login} /> */}
+                            {/* <Route exact path='/profile' component={Profile} /> */}
+                            {/* <Route exact path='/donate' component={Donate} /> */}
+                            {/* <Route component={NoMatch} /> */}
+                        </Switch>
+                    </StoreProvider>
+                </div>
+            </Router>
+        </ApolloProvider>
+    )
+};
+
+export default App;
--- client/src/components/SignupForm/index.js
+++ client/src/components/SignupForm/index.js
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
 import { useMutation } from '@apollo/react-hooks';
-import { ADD_USER } from '../utils/mutations';
+import { ADD_USER } from '../../utils/mutations';
 
-import Auth from '../utils/auth';
+import Auth from '../../utils/auth';
 
 const SignupForm = () => {
   // set initial form state
