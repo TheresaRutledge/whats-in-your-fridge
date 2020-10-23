b6520cbfb84bd94aa711fbdfe4caa123526574d2
Walker-Walker <awalker0@gmail.com>
2020-10-22 19:04:26 -0700

    added more mutations and queries, confused on syntax for front end mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -2,7 +2,7 @@
 import gql from 'graphql-tag';
 
 export const LOGIN = gql `
-mutation login($email:String!, $password: String!) {
+mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
         token
         user {
@@ -13,4 +13,29 @@ mutation login($email:String!, $password: String!) {
 
 `;
 
-export const ADD_
+export const ADD_USER = gql `
+mutation addUser($email: String!, $password: String!, $username: String!){
+    addUser(email: $email, password: $password, username: $username) {
+        token
+        user {
+            _id
+        }
+    }
+}
+
+`
+
+export const ADD_COMMENT = gql `
+mutation addComment($commentText: Strinq!, recipeId: String!) {
+    addComment(commentText: $commentText) {
+        
+    }
+}
+
+
+`
+
+export const UPDATE_COMMENT = gql ``
+
+export const DELETE_COMMENT = gql ``
+
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,29 @@
 //alex
+import gql from 'graphql-tag';
+
+export const QUERY_USER = gql `
+{
+    me {
+        userName
+        email
+        favorites
+    }
+}
+
+`;
+
+export const QUERY_COMMENT = gql `
+{
+    comment {
+        commentText
+        recipeId
+        createAt
+        user {
+            User
+        }
+    }
+}
+`
+// query getComment(id:String) {
+// //     commentbyRecipeId(comment:$comment)
+// // }
