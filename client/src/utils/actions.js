38a5130f7ca7b3dc91d475f0fd364511b177ef58
Walker-Walker <awalker0@gmail.com>
2020-10-22 17:03:12 -0700

    I had to delete and move some files from utils server to utils client accidentally starting writing in the wrong directory

--- client/src/utils/actions.js
+++ client/src/utils/actions.js
@@ -1,1 +1,4 @@
-// Theresa
+export const ADD_USER = "ADD_USER";
+export const ADD_COMMENT = "ADD_COMMENT";
+export const UPDATE_COMMENT = "ADD_COMMENT";
+export const DELETE_COMMENT = "DELETE_COMMENT";
--- server/utils/actions.js
+++
@@ -1,4 +0,0 @@
-export const ADD_USER = "UPDATE_USER";
-export const ADD_COMMENT = "ADD_COMMENT";
-export const UPDATE_COMMENT = "ADD_COMMENT";
-export const DELETE_COMMENT = "DELETE_COMMENT";
