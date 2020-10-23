c88f44769b07828a7d00fcb008f4974acc4f3908
Walker-Walker <awalker0@gmail.com>
2020-10-22 17:10:08 -0700

    started on mutations.js added login mutation reference to token and user _id, since i am working on non resolvers I will checkout to diff branch

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,14 @@
 // Alex
+import gql from 'graphql-tag';
+
+export const LOGIN = gql `
+mutation login($email:String!, $password: String!) {
+    login(email: $email, password: $password) {
+        token
+        user {
+            _id
+        }
+    }
+}
+
+`;
